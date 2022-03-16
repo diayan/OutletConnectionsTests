ffc528a88118e00a33dfad9e20e2c07d8e5ba266
diayan <diayans@outlook.com>
2022-03-16 22:23:23 +0000

    change access control on outlets & write tests to ensure that they are not nil

--- OutletConnectionsTests/OutletConnectionsViewControllerTests.swift
+++ OutletConnectionsTests/OutletConnectionsViewControllerTests.swift
@@ -6,10 +6,16 @@
 //
 
 import XCTest
+@testable import OutletConnections
 
 class OutletConnectionsViewControllerTests: XCTestCase {
     
-    func test_zero() {
-        XCTFail("Tests not yet implemented")
+    func test_outlets_shouldBeConnected() {
+        let sut = OutletConnectionsViewController() //load view controller
+        sut.loadViewIfNeeded()
+        
+        //these assertions checks to make sure that these views (label and button) are not nil
+        XCTAssertNotNil(sut.label, "label")
+        XCTAssertNotNil(sut.button, "button")
     }
 }
