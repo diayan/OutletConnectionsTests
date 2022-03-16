aee648340848d25e5742faecdb1c1791bb8b403f
diayan <diayans@outlook.com>
2022-03-16 22:09:38 +0000

    start with test zero to make sure that the test fails

---
+++ OutletConnectionsTests/OutletConnectionsViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  OutletConnectionsViewControllerTests.swift
+//  OutletConnectionsTests
+//
+//  Created by diayan siat on 16/03/2022.
+//
+
+import XCTest
+
+class OutletConnectionsViewControllerTests: XCTestCase {
+    
+    func test_zero() {
+        XCTFail("Tests not yet implemented")
+    }
+}
