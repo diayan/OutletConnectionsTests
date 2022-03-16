65aeac274f7b923c267a183d4cfaf686804b4b4c
diayan <diayans@outlook.com>
2022-03-16 22:04:31 +0000

    add IBOutlets to view controller to test them

---
+++ OutletConnections/OutletConnectionsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OutletConnectionsViewController.swift
+//  OutletConnections
+//
+//  Created by diayan siat on 16/03/2022.
+//
+
+import UIKit
+
+class OutletConnectionsViewController: UIViewController {
+
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
